c9298e2ec1f86265675202ba33d1639442863c02
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2024-05-10 08:47:55 +0200

    Add files via upload

---
+++ Flatbökuveisla/Flatbökuveisla/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pizza
+
+{
+    class Flatbökuveisla
+    {
+        static void Main(string[] args)
+        {
+            // number of pizza slices is "n"
+            // number of members in the household are "m"
+
+            Console.WriteLine("Enter the number of Pizza slices that was delivered:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of people that will eat the pizza:");
+            int m = int.Parse(Console.ReadLine());
+
+            if (n % m == 0)
+            {
+                Console.WriteLine("Everyone is happy");
+
+            }
+            else
+            {
+                Console.WriteLine(n % m);
+            }
+        }
+    }
+}
